1fff12cfe1c16a898917b80c6e9c338d8b0922fe
msadaniowski <78354043+msadaniowski@users.noreply.github.com>
2021-06-24 19:09:56 -0300

    Add files via upload

--- custom_account_invoice/models/models.py
+++ custom_account_invoice/models/models.py
@@ -8,4 +8,4 @@ class AccountInherit(models.Model):
 
     # main_seller_id = fields.Many2one(related='test.product_id.main_seller_id', readonly=True, string='Proveedor')
     main_seller_ids = fields.Many2one('res.partner')
-    nro_viaje = fields.Char(related='main_seller_ids.product_id.main_seller_id', string='Proveedor')
+    main_sellers_id = fields.Char(related='main_seller_ids.product_id.main_seller_id', string='Proveedor')

5d8716cb3cd6c905c93c471119e1c1a77ffe69fc
msadaniowski <78354043+msadaniowski@users.noreply.github.com>
2022-03-11 14:07:39 -0300

    Add files via upload

---
+++ stock_report/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "stock_report",
+
+    'summary': """
+        """,
+
+    'description': """
+          """,
+
+    'author': "Sadaniowski Mauricio",
+    'website': "http://www.odoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '14.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['stock'],
+
+    # always loaded
+    'data': [
+        'views/stock_report.xml',
+        'views/stock_report_views.xml',
+    ],
+}

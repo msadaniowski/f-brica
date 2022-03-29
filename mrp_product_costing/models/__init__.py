9aad6ab4666c167e26e905efb3c2f690d48e58d1
msadaniowski <78354043+msadaniowski@users.noreply.github.com>
2022-03-29 13:16:19 -0300

    Add files via upload

---
+++ mrp_product_costing/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from . import mrp_product_costing_parameters
+from . import mrp_bom
+from . import mrp_production
+from . import mrp_actual_posting
+from . import mrp_economical_closure
